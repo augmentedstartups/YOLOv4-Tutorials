06aa2122563b57f02904533839278b567b98d44c
Augmented Startups <reigngt09@gmail.com>
2020-05-25 08:08:51 +0200

    Add files via upload

---
+++ 4. App 1 Social Distancing App/App1_Social_Distance.py
@@ -0,0 +1,212 @@
+#================================================================
+#  To learn how to Develop Advance YOLOv4 Apps - Then check out:
+#  https://augmentedstartups.info/yolov4release
+#================================================================ 
+from ctypes import *
+import math
+import random
+import os
+import cv2
+import numpy as np
+import time
+import darknet
+from itertools import combinations
+
+
+def is_close(p1, p2):
+    """
+    #================================================================
+    # 1. Purpose : Calculate Euclidean Distance between two points
+    #================================================================    
+    :param:
+    p1, p2 = two points for calculating Euclidean Distance
+
+    :return:
+    dst = Euclidean Distance between two 2d points
+    """
+    dst = math.sqrt(p1**2 + p2**2)
+    #=================================================================#
+    return dst 
+
+
+def convertBack(x, y, w, h): 
+    #================================================================
+    # 2.Purpose : Converts center coordinates to rectangle coordinates
+    #================================================================  
+    """
+    :param:
+    x, y = midpoint of bbox
+    w, h = width, height of the bbox
+    
+    :return:
+    xmin, ymin, xmax, ymax
+    """
+    xmin = int(round(x - (w / 2)))
+    xmax = int(round(x + (w / 2)))
+    ymin = int(round(y - (h / 2)))
+    ymax = int(round(y + (h / 2)))
+    return xmin, ymin, xmax, ymax
+
+
+def cvDrawBoxes(detections, img):
+    """
+    :param:
+    detections = total detections in one frame
+    img = image from detect_image method of darknet
+
+    :return:
+    img with bbox
+    """
+    #================================================================
+    # 3.1 Purpose : Filter out Persons class from detections and get 
+    #           bounding box centroid for each person detection.
+    #================================================================
+    if len(detections) > 0:  						# At least 1 detection in the image and check detection presence in a frame  
+        centroid_dict = dict() 						# Function creates a dictionary and calls it centroid_dict
+        objectId = 0								# We inialize a variable called ObjectId and set it to 0
+        for detection in detections:				# In this if statement, we filter all the detections for persons only
+            # Check for the only person name tag 
+            name_tag = str(detection[0].decode())   # Coco file has string of all the names
+            if name_tag == 'person':                
+                x, y, w, h = detection[2][0],\
+                            detection[2][1],\
+                            detection[2][2],\
+                            detection[2][3]      	# Store the center points of the detections
+                xmin, ymin, xmax, ymax = convertBack(float(x), float(y), float(w), float(h))   # Convert from center coordinates to rectangular coordinates, We use floats to ensure the precision of the BBox            
+                # Append center point of bbox for persons detected.
+                centroid_dict[objectId] = (int(x), int(y), xmin, ymin, xmax, ymax) # Create dictionary of tuple with 'objectId' as the index center points and bbox
+                objectId += 1 #Increment the index for each detection      
+    #=================================================================#
+    
+    #=================================================================
+    # 3.2 Purpose : Determine which person bbox are close to each other
+    #=================================================================            	
+        red_zone_list = [] # List containing which Object id is in under threshold distance condition. 
+        red_line_list = []
+        for (id1, p1), (id2, p2) in combinations(centroid_dict.items(), 2): # Get all the combinations of close detections, #List of multiple items - id1 1, points 2, 1,3
+            dx, dy = p1[0] - p2[0], p1[1] - p2[1]  	# Check the difference between centroid x: 0, y :1
+            distance = is_close(dx, dy) 			# Calculates the Euclidean distance
+            if distance < 75.0:						# Set our social distance threshold - If they meet this condition then..
+                if id1 not in red_zone_list:
+                    red_zone_list.append(id1)       #  Add Id to a list
+                    red_line_list.append(p1[0:2])   #  Add points to the list
+                if id2 not in red_zone_list:
+                    red_zone_list.append(id2)		# Same for the second id 
+                    red_line_list.append(p2[0:2])
+        
+        for idx, box in centroid_dict.items():  # dict (1(key):red(value), 2 blue)  idx - key  box - value
+            if idx in red_zone_list:   # if id is in red zone list
+                cv2.rectangle(img, (box[2], box[3]), (box[4], box[5]), (255, 0, 0), 2) # Create Red bounding boxes  #starting point, ending point size of 2
+            else:
+                cv2.rectangle(img, (box[2], box[3]), (box[4], box[5]), (0, 255, 0), 2) # Create Green bounding boxes
+		#=================================================================#
+
+		#=================================================================
+    	# 3.3 Purpose : Display Risk Analytics and Show Risk Indicators
+    	#=================================================================        
+        text = "People at Risk: %s" % str(len(red_zone_list)) 			# Count People at Risk
+        location = (10,25)												# Set the location of the displayed text
+        cv2.putText(img, text, location, cv2.FONT_HERSHEY_SIMPLEX, 1, (246,86,86), 2, cv2.LINE_AA)  # Display Text
+
+        for check in range(0, len(red_line_list)-1):					# Draw line between nearby bboxes iterate through redlist items
+            start_point = red_line_list[check] 
+            end_point = red_line_list[check+1]
+            check_line_x = abs(end_point[0] - start_point[0])   		# Calcualte the line coordinates for x  
+            check_line_y = abs(end_point[1] - start_point[1])			# Calcualte the line coordinates for y
+            if (check_line_x < 75) and (check_line_y < 25):				# If both are We check that the lines are below our threshold distance.
+                cv2.line(img, start_point, end_point, (255, 0, 0), 2)   # Only above the treshold lines are displayed. 
+        #=================================================================#
+    return img
+
+
+netMain = None
+metaMain = None
+altNames = None
+
+
+def YOLO():
+    """
+    Perform Object detection
+    """
+    global metaMain, netMain, altNames
+    configPath = "./cfg/yolov4.cfg"
+    weightPath = "./yolov4.weights"
+    metaPath = "./cfg/coco.data"
+    if not os.path.exists(configPath):
+        raise ValueError("Invalid config path `" +
+                         os.path.abspath(configPath)+"`")
+    if not os.path.exists(weightPath):
+        raise ValueError("Invalid weight path `" +
+                         os.path.abspath(weightPath)+"`")
+    if not os.path.exists(metaPath):
+        raise ValueError("Invalid data file path `" +
+                         os.path.abspath(metaPath)+"`")
+    if netMain is None:
+        netMain = darknet.load_net_custom(configPath.encode(
+            "ascii"), weightPath.encode("ascii"), 0, 1)  # batch size = 1
+    if metaMain is None:
+        metaMain = darknet.load_meta(metaPath.encode("ascii"))
+    if altNames is None:
+        try:
+            with open(metaPath) as metaFH:
+                metaContents = metaFH.read()
+                import re
+                match = re.search("names *= *(.*)$", metaContents,
+                                  re.IGNORECASE | re.MULTILINE)
+                if match:
+                    result = match.group(1)
+                else:
+                    result = None
+                try:
+                    if os.path.exists(result):
+                        with open(result) as namesFH:
+                            namesList = namesFH.read().strip().split("\n")
+                            altNames = [x.strip() for x in namesList]
+                except TypeError:
+                    pass
+        except Exception:
+            pass
+    #cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture("./Input/test5.mp4")
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+    new_height, new_width = frame_height // 2, frame_width // 2
+    # print("Video Reolution: ",(width, height))
+
+    out = cv2.VideoWriter(
+            "./Demo/test5_output.avi", cv2.VideoWriter_fourcc(*"MJPG"), 10.0,
+            (new_width, new_height))
+    
+    # print("Starting the YOLO loop...")
+
+    # Create an image we reuse for each detect
+    darknet_image = darknet.make_image(new_width, new_height, 3)
+    
+    while True:
+        prev_time = time.time()
+        ret, frame_read = cap.read()
+        # Check if frame present :: 'ret' returns True if frame present, otherwise break the loop.
+        if not ret:
+            break
+
+        frame_rgb = cv2.cvtColor(frame_read, cv2.COLOR_BGR2RGB)
+        frame_resized = cv2.resize(frame_rgb,
+                                   (new_width, new_height),
+                                   interpolation=cv2.INTER_LINEAR)
+
+        darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
+
+        detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.25)
+        image = cvDrawBoxes(detections, frame_resized)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        print(1/(time.time()-prev_time))
+        cv2.imshow('Demo', image)
+        cv2.waitKey(3)
+        out.write(image)
+
+    cap.release()
+    out.release()
+    print(":::Video Write Completed")
+
+if __name__ == "__main__":
+    YOLO()
---
+++ 4. App 1 Social Distancing App/App1_Social_Distance_base.py
@@ -0,0 +1,171 @@
+#================================================================
+#  To learn how to Develop Advance YOLOv4 Apps - Then check out:
+#  https://augmentedstartups.info/yolov4release
+#================================================================ 
+from ctypes import *
+import math
+import random
+import os
+import cv2
+import numpy as np
+import time
+import darknet
+from itertools import combinations
+
+
+def is_close(p1, p2):
+    """
+    #================================================================
+    # 1. Purpose : Calculate Euclidean Distance between two points
+    #================================================================    
+    :param:
+    p1, p2 = two points for calculating Euclidean Distance
+
+    :return:
+    dst = Euclidean Distance between two 2d points
+    """
+    
+    #=================================================================#
+    return dst 
+
+
+def convertBack(x, y, w, h): 
+    #================================================================
+    # 2.Purpose : Converts center coordinates to rectangle coordinates
+    #================================================================  
+    """
+    :param:
+    x, y = midpoint of bbox
+    w, h = width, height of the bbox
+    
+    :return:
+    xmin, ymin, xmax, ymax
+    """
+    xmin = int(round(x - (w / 2)))
+    xmax = int(round(x + (w / 2)))
+    ymin = int(round(y - (h / 2)))
+    ymax = int(round(y + (h / 2)))
+    return xmin, ymin, xmax, ymax
+
+
+def cvDrawBoxes(detections, img):
+    """
+    :param:
+    detections = total detections in one frame
+    img = image from detect_image method of darknet
+
+    :return:
+    img with bbox
+    """
+    #================================================================
+    # 3.1 Purpose : Filter out Persons class from detections and get 
+    #           bounding box centroid for each person detection.
+    #================================================================
+
+    #=================================================================#
+    
+    #=================================================================
+    # 3.2 Purpose : Determine which person bbox are close to each other
+    #=================================================================            	
+
+		#=================================================================#
+
+		#=================================================================
+    	# 3.3 Purpose : Display Risk Analytics and Show Risk Indicators
+    	#=================================================================        
+
+        #=================================================================#
+    return img
+
+
+netMain = None
+metaMain = None
+altNames = None
+
+
+def YOLO():
+    """
+    Perform Object detection
+    """
+    global metaMain, netMain, altNames
+    configPath = "./cfg/yolov4.cfg"
+    weightPath = "./yolov4.weights"
+    metaPath = "./cfg/coco.data"
+    if not os.path.exists(configPath):
+        raise ValueError("Invalid config path `" +
+                         os.path.abspath(configPath)+"`")
+    if not os.path.exists(weightPath):
+        raise ValueError("Invalid weight path `" +
+                         os.path.abspath(weightPath)+"`")
+    if not os.path.exists(metaPath):
+        raise ValueError("Invalid data file path `" +
+                         os.path.abspath(metaPath)+"`")
+    if netMain is None:
+        netMain = darknet.load_net_custom(configPath.encode(
+            "ascii"), weightPath.encode("ascii"), 0, 1)  # batch size = 1
+    if metaMain is None:
+        metaMain = darknet.load_meta(metaPath.encode("ascii"))
+    if altNames is None:
+        try:
+            with open(metaPath) as metaFH:
+                metaContents = metaFH.read()
+                import re
+                match = re.search("names *= *(.*)$", metaContents,
+                                  re.IGNORECASE | re.MULTILINE)
+                if match:
+                    result = match.group(1)
+                else:
+                    result = None
+                try:
+                    if os.path.exists(result):
+                        with open(result) as namesFH:
+                            namesList = namesFH.read().strip().split("\n")
+                            altNames = [x.strip() for x in namesList]
+                except TypeError:
+                    pass
+        except Exception:
+            pass
+    #cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture("./Input/test5.mp4")   # <----- Replace with your video directory
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+    new_height, new_width = frame_height // 2, frame_width // 2
+    # print("Video Reolution: ",(width, height))
+
+    out = cv2.VideoWriter(
+            "./Demo/test5_output.avi", cv2.VideoWriter_fourcc(*"MJPG"), 10.0, # <----- Replace with your output directory
+            (new_width, new_height))
+    
+    # print("Starting the YOLO loop...")
+
+    # Create an image we reuse for each detect
+    darknet_image = darknet.make_image(new_width, new_height, 3)
+    
+    while True:
+        prev_time = time.time()
+        ret, frame_read = cap.read()
+        # Check if frame present :: 'ret' returns True if frame present, otherwise break the loop.
+        if not ret:
+            break
+
+        frame_rgb = cv2.cvtColor(frame_read, cv2.COLOR_BGR2RGB)
+        frame_resized = cv2.resize(frame_rgb,
+                                   (new_width, new_height),
+                                   interpolation=cv2.INTER_LINEAR)
+
+        darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
+
+        detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.25)
+        image = cvDrawBoxes(detections, frame_resized)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        print(1/(time.time()-prev_time))
+        cv2.imshow('Demo', image)
+        cv2.waitKey(3)
+        out.write(image)
+
+    cap.release()
+    out.release()
+    print(":::Video Write Completed")
+
+if __name__ == "__main__":
+    YOLO()
